2cf5ed46c4f4538584d45d275e78fc049ff8470a
MscrmTools <tanguy92@hotmail.com>
2021-02-23 18:19:36 +0100

    First commit

---
+++ MscrmTools.EnvironmentVariableManager/MscrmTools.EnvironmentVariableManager/EvmControl.cs
@@ -0,0 +1,212 @@
+﻿using McTools.Xrm.Connection;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+using XrmToolBox.Extensibility.Interfaces;
+
+namespace MscrmTools.EnvironmentVariableManager
+{
+    public partial class EvmControl : PluginControlBase, IGitHubPlugin, IPayPalPlugin
+    {
+        private readonly List<int> _rowsIndexChanged = new List<int>();
+
+        public EvmControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MyPluginControl_Load(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+
+            if (newService != null)
+            {
+                LoadVariables();
+            }
+        }
+
+        private void toolStripMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem == tsbLoad)
+            {
+                ExecuteMethod(LoadVariables);
+            }
+        }
+
+        private void LoadVariables()
+        {
+            var variables = Service.RetrieveMultiple(new QueryExpression("environmentvariablevalue")
+            {
+                NoLock = true,
+                ColumnSet = new ColumnSet("value"),
+                LinkEntities =
+                {
+                    new LinkEntity
+                    {
+                        LinkFromEntityName = "environmentvariablevalue",
+                        LinkFromAttributeName = "environmentvariabledefinitionid",
+                        LinkToAttributeName = "environmentvariabledefinitionid",
+                        LinkToEntityName = "environmentvariabledefinition",
+                        Columns = new ColumnSet("displayname", "schemaname"),
+                        EntityAlias = "def",
+                        Orders = { new OrderExpression("displayname", OrderType.Ascending) }
+                    }
+                }
+            });
+
+            var table = new DataTable();
+            table.Columns.Add(new DataColumn("Variable name") { ReadOnly = true });
+            table.Columns.Add(new DataColumn("Schema name") { ReadOnly = true });
+            table.Columns.Add(new DataColumn("Value"));
+            table.Columns.Add(new DataColumn("Id") { ReadOnly = true });
+
+            foreach (var variable in variables.Entities)
+            {
+                table.Rows.Add(
+                    variable.GetAttributeValue<AliasedValue>("def.displayname").Value.ToString(),
+                    variable.GetAttributeValue<AliasedValue>("def.schemaname").Value.ToString(),
+                    variable.GetAttributeValue<string>("value"),
+                    variable.Id
+                );
+            }
+
+            dataGridView1.DataSource = table;
+            SetDatagridViewColumnsSettings();
+            dataGridView1.CellValueChanged += DataGridView1_CellValueChanged;
+        }
+
+        private void DataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            _rowsIndexChanged.Add(e.RowIndex);
+
+            foreach (DataGridViewCell cell in dataGridView1.Rows[e.RowIndex].Cells)
+            {
+                cell.Style.BackColor = Color.Yellow;
+            }
+        }
+
+        private void tsbUpdate_Click(object sender, EventArgs e)
+        {
+            if (_rowsIndexChanged.Count == 0)
+            {
+                MessageBox.Show(this, @"No variable was changed", @"Warning", MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (MessageBox.Show(this, @"Are you sure you want to update these variables?", @"Question",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                return;
+
+            var list = new List<Tuple<Guid, string, string, int>>();
+
+            foreach (var index in _rowsIndexChanged)
+            {
+                var row = dataGridView1.Rows[index];
+                var variable = row.Cells[0].Value.ToString();
+                var id = new Guid(row.Cells[3].Value.ToString());
+                var value = row.Cells[2].Value.ToString();
+
+                list.Add(new Tuple<Guid, string, string, int>(id, variable, value, index));
+            }
+
+            WorkAsync(new WorkAsyncInfo
+            {
+                Work = (bw, evt) =>
+                {
+                    var errors = new Dictionary<string, string>();
+
+                    foreach (var item in list)
+                    {
+                        bw.ReportProgress(0, $"Updating variable '{item.Item2}'");
+
+                        try
+                        {
+                            Service.Update(new Entity("environmentvariablevalue")
+                            {
+                                Id = item.Item1,
+                                Attributes =
+                                {
+                                    {"value", item.Item3}
+                                }
+                            });
+
+                            _rowsIndexChanged.Remove(item.Item4);
+
+                            Invoke(new Action(() =>
+                            {
+                                foreach (DataGridViewCell cell in dataGridView1.Rows[item.Item4].Cells)
+                                {
+                                    cell.Style.BackColor = Color.Green;
+                                }
+                            }));
+                        }
+                        catch (Exception error)
+                        {
+                            errors.Add(item.Item2, error.Message);
+                        }
+                    }
+
+                    evt.Result = errors;
+                },
+                PostWorkCallBack = evt =>
+                {
+                    var errors = (Dictionary<string, string>)evt.Result;
+                    if (errors.Count > 0)
+                    {
+                        MessageBox.Show(this, $@"The following error(s) occur(s):
+{string.Join("\r\n- ", errors.Select(error => $"{error.Key}: {error.Value}"))}", @"Error", MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                    }
+
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+
+                        foreach (DataGridViewCell cell in row.Cells)
+                        {
+                            if (cell.Style.BackColor == Color.Green)
+                                cell.Style.BackColor = Color.White;
+                        }
+                },
+                ProgressChanged = evt =>
+                {
+                    SetWorkingMessage(evt.UserState.ToString());
+                }
+            });
+        }
+
+        private void SetDatagridViewColumnsSettings()
+        {
+            dataGridView1.Columns[0].Width = 200;
+            dataGridView1.Columns[1].Width = 200;
+            dataGridView1.Columns[2].Width = 600;
+            dataGridView1.Columns[3].Visible = false;
+        }
+
+        #region Github
+
+        public string RepositoryName => "MscrmTools.EnvironmentVariableManager";
+        public string UserName => "MscrmTools";
+
+        #endregion Github
+
+        #region PayPal
+
+        public string DonationDescription => "Donation for Environment Variable Manager (XrmToolBox)";
+        public string EmailAccount => "tanguy92@hotmail.com";
+
+        #endregion PayPal
+    }
+}
---
+++ MscrmTools.EnvironmentVariableManager/MscrmTools.EnvironmentVariableManager/EvmControl.designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace MscrmTools.EnvironmentVariableManager
+{
+    partial class EvmControl
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.tsbLoad = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbUpdate = new System.Windows.Forms.ToolStripButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStripMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStripMenu
+            // 
+            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbLoad,
+            this.toolStripSeparator1,
+            this.tsbUpdate});
+            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Size = new System.Drawing.Size(1367, 46);
+            this.toolStripMenu.TabIndex = 4;
+            this.toolStripMenu.Text = "toolStrip1";
+            this.toolStripMenu.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStripMenu_ItemClicked);
+            // 
+            // tsbLoad
+            // 
+            this.tsbLoad.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbLoad.Name = "tsbLoad";
+            this.tsbLoad.Size = new System.Drawing.Size(234, 29);
+            this.tsbLoad.Text = "Load Environment variables";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 34);
+            // 
+            // tsbUpdate
+            // 
+            this.tsbUpdate.Image = global::MscrmTools.EnvironmentVariableManager.Properties.Resources.Save_16;
+            this.tsbUpdate.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbUpdate.Name = "tsbUpdate";
+            this.tsbUpdate.Size = new System.Drawing.Size(98, 29);
+            this.tsbUpdate.Text = "Update";
+            this.tsbUpdate.Click += new System.EventHandler(this.tsbUpdate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1367, 781);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // EvmControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.toolStripMenu);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "EvmControl";
+            this.Size = new System.Drawing.Size(1139, 697);
+            this.Load += new System.EventHandler(this.MyPluginControl_Load);
+            this.toolStripMenu.ResumeLayout(false);
+            this.toolStripMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbLoad;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripButton tsbUpdate;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
